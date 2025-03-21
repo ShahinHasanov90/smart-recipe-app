70c796d12b4c9724976170b69493b3d0b7a5ba0d
Shahin_Hasanov <113840366+ShahinHasanov90@users.noreply.github.com>
2025-03-21 23:11:27 +0400

    Ana aktivite eklendi

---
+++ app/src/main/java/com/smartrecipe/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.smartrecipe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.smartrecipe.ui.theme.SmartRecipeTheme
+import com.smartrecipe.ui.navigation.AppNavigation
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SmartRecipeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation()
+                }
+            }
+        }
+    }
+}
